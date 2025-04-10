7e9d1beb16b926136ac71fd8ff7dc617254ffa2f
11Pk <prabhukaur011@gmail.com>
2025-04-10 17:19:17 +0530

    .

--- hackathon.js
+++ hackathon.js
@@ -50,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(user)
     if (!user) {
       alert("Please fill the sign-in form.");
+
       return;
     }
 
@@ -86,4 +87,109 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("Sorry we coudn't load your request at the moment.");
       });
   })
+
+  let takeform = document.querySelector(".takeform");
+
+  takeform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const user = firebase.auth().currentUser;
+    if (!user) {
+      alert("Please fill the sign-in form.");
+      return;
+    }
+    let book = takeform.querySelector(".input-group.book");
+    let bookname = book.value;
+    let address=takeform.querySelector(".input-group.location")
+    db.ref("take")
+      .push({
+        name: bookname,
+        username: user.uid,
+        location:address,
+        lat:getCoordinates().lat,
+        long:getCoordinates().long
+      })
+      .then(() => {
+        alert("We'll get back to you once we find a suitable donor.");
+        takeform.reset();
+      })
+
+      .catch((error) => {
+        alert("Sorry we coudn't load your request at the moment.");
+      });
+  });
+  const resultsgive = [];
+  const resultstake = [];
+  const giveref = db.ref("donate");
+  const takeref = db.ref("take");
+
+
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; 
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c; 
+  }
+  takeref.on("child_added", (snapshot) => {
+    const takedata = snapshot.val();
+    if (!takedata || !takedata.name) return;
+    giveref.once("value").then((snapshot) => {
+      const givedata = snapshot.val();
+      if (!givedata) return;
+      for (let key in givedata) {
+        if (givedata[key].name === takedata.name) {
+          resultsgive.push(givedata[key]);
+          resultstake.push(takedata);
+        }
+      }
+      if(resultsgive.length>1)
+      {
+      let min_dist=calculateDistance(resultsgive[0].lat,resultsgive[0].long,resultstake[0].long,resultstake[0].lat)
+      let min_index=0
+      for (let i=1;i<resultsgive.length;i++)
+      {
+        if(calculateDistance(resultsgive[i].lat,resultsgive[i].long,resultstake[i].long,resultstake[i].lat)<min_dist)
+        {
+          min_dist=calculateDistance(resultsgive[i].lat,resultsgive[i].long,resultstake[i].long,resultstake[i].lat)
+          min_index=i
+        }
+      }
+    }
+      
+    });
+  });
+  giveref.on("child_added", (snapshot) => {
+    const givedata = snapshot.val();
+    if (!givedata || !givedata.name) return;
+    takeref.once("value").then((snapshot) => {
+      const takedata = snapshot.val();
+      if (!takedata) return;
+      for (let key in takedata) {
+        if (takedata[key].name === givedata.name) {
+          resultsgive.push(givedata);
+          resultstake.push(takedata[key]);
+        }
+        if(resultsgive.length>1)
+          {
+          let min_dist=calculateDistance(resultsgive[0].lat,resultsgive[0].long,resultstake[0].long,resultstake[0].lat)
+          let min_index=0
+          for (let i=1;i<resultsgive.length;i++)
+          {
+            if(calculateDistance(resultsgive[i].lat,resultsgive[i].long,resultstake[i].long,resultstake[i].lat)<min_dist)
+            {
+              min_dist=calculateDistance(resultsgive[i].lat,resultsgive[i].long,resultstake[i].long,resultstake[i].lat)
+              min_index=i
+            }
+          }
+        }
+      }
+      
+    });
+  });
 })
